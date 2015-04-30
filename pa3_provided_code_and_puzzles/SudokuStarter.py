1ff53ee509ddafea32e24e3592d3e394791dcb68
Collin Barnwell <cbarnwell@g2crowd.com>
2015-04-30 14:32:33 -0500

    board representation all good

--- pa3_provided_code_and_puzzles/SudokuStarter.py
+++ pa3_provided_code_and_puzzles/SudokuStarter.py
@@ -54,65 +54,6 @@ class SudokuBoard:
             else:
                 print sep
 
-class boardCell:
-    """Holds the necessary information about a cell"""
-    def __init__(self, BoardSize, value, section):
-        self.cellVal = value
-        self.possibleVals = set(range(1, BoardSize))
-        self.previouslyTried = 0
-        self.boardSection = section
-
-    def setCell(self, value):
-        self.cellVal = value
-
-    def removePossibleVal(self, value):
-        if value in self.possibleVals:
-            self.possibleVals.remove(value)
-
-
-
-class MyBoard:
-    def __init__(self, sb, bsize):
-        self.boardSize = bsize
-        self.board = []
-        subsquare = int(math.sqrt(bsize))
-        #Make empty board of cells with all 0's
-        for i in range(0, bsize):
-            self.board.append([])
-            for j in range(0, bsize):
-                boardCol = j // subsquare
-                boardRow = i // subsquare
-                boardSection =  boardRow * subsquare + boardCol
-                self.board[i].append(boardCell(bsize, 0, boardSection))
-        #Search through board of ints for non-zero values, and set board
-        for i in range(0, bsize):
-            for j in range(0, bsize):
-                if sb[i][j] != 0:
-                    self.addValue(sb[i][j], i, j)
-
-
-    def addValue(self, value, row, col):
-        self.board[row][col].setCell(value)
-        for i in range(BoardSize):
-            if i != col:
-                self.board[row][i].removePossibleVal(value)
-            if i != row:
-                self.board[i][col].removePossibleVal(value)
-        subsquareMates = self.getSubsquareMates(self.board[row][col])
-        for tile in subsquareMates:
-            tile.removePossibleVal(value)
-
-
-
-    def getSubsquareMates(self, cell):
-        currentSection = cell.boardSection
-        subsquareMates = []
-        for i in range(self.boardSize):
-            for tile in self.board[i]:
-                if tile.boardSection == currentSection:
-                    subsquareMates.append(tile)
-        return subsquareMates
-
 def parse_file(filename):
     """Parses a sudoku text file into a BoardSize, and a 2d array which holds
     the value of each cell. Array elements holding a 0 are considered to be
---
+++ pa3_provided_code_and_puzzles/board.py
@@ -0,0 +1,71 @@
+from SudokuStarter import *
+
+class boardCell:
+    """Holds the necessary information about a cell"""
+    def __init__(self, BoardSize, value, section):
+        self.cellVal = value
+        self.possibleVals = set(range(1, BoardSize+1))
+        self.previouslyTried = 0
+        self.boardSection = section
+
+    def setCell(self, value):
+        self.cellVal = value
+
+    def removePossibleVal(self, value):
+        if value in self.possibleVals:
+            self.possibleVals.remove(value)
+
+class MyBoard:
+    def __init__(self, sb, bsize):
+        self.boardSize = bsize
+        self.board = []
+        subsquare = int(math.sqrt(bsize))
+        #Make empty board of cells with all 0's
+        for i in range(0, bsize):
+            self.board.append([])
+            for j in range(0, bsize):
+                boardCol = j // subsquare
+                boardRow = i // subsquare
+                boardSection =  boardRow * subsquare + boardCol
+                self.board[i].append(boardCell(bsize, 0, boardSection))
+        #Search through board of ints for non-zero values, and set board
+        for i in range(0, bsize):
+            for j in range(0, bsize):
+                if sb[i][j] != 0:
+                    self.addValue(sb[i][j], i, j)
+
+    def addValue(self, value, row, col):
+        self.board[row][col].setCell(value)
+        for i in range(self.boardSize):
+            if i != col:
+                self.board[row][i].removePossibleVal(value)
+            if i != row:
+                self.board[i][col].removePossibleVal(value)
+        subsquareMates = self.getSubsquareMates(self.board[row][col])
+        for tile in subsquareMates:
+            tile.removePossibleVal(value)
+
+    def getSubsquareMates(self, cell):
+        currentSection = cell.boardSection
+        subsquareMates = []
+        for i in range(self.boardSize):
+            for tile in self.board[i]:
+                if tile.boardSection == currentSection:
+                    subsquareMates.append(tile)
+        return subsquareMates
+
+    def p(self):
+        for i in range(self.boardSize):
+            for j in range(self.boardSize):
+                if self.board[i][j].cellVal:
+                    print self.board[i][j].cellVal
+                else:
+                    print "(" + str(self.board[i][j].possibleVals) + ")"
+            print "\n"
+
+
+def boardTest(file):
+    sb = parse_file(file)
+    m = MyBoard(sb, len(sb))
+    m.p()
+
